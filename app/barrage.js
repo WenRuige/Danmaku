3507cd77e155015a4f6cdf8b66819f240138be2b
gewenrui <gewenrui@didichuxing.com>
2017-10-16 17:06:37 +0800

    update

--- app/barrage.js
+++ app/barrage.js
@@ -1,23 +1,25 @@
 // 加载config.json   ./ -> 上级  ../ -> 上上级
 const offlineDanmu = require("../danmu.json");
-let config = {
-    'text': '',
-    'color': ''
-};
+
 //直接执行导入的方法
-barrage = function () {
+barrage = function (config) {
     let node = 0;  //节点
-    let flag = 0;
     //初始化
-    core = (config) => {
-        // let dom = this.getCanvas();
+    core = () => {
         let obj = createObj();
         addAnimationRule();
         addStyle(obj, config);
         document.getElementById("danmu").appendChild(obj);   //追加对象
+        document.getElementById(obj.id).appendChild(addImg());
         obj.addEventListener("animationend", animationend.bind(this), false);    //监听css动画结束回调
         obj.style.webkitAnimationName = 'animation';    //加动画
         obj.className += "animation";
+    };
+    addImg = () => {
+        let imgNode = document.createElement('img');
+        imgNode.setAttribute('src', config.pic);
+        imgNode.className += "img";
+        return imgNode;
     };
     createObj = () => {
         let obj = document.createElement('div');
@@ -25,33 +27,34 @@ barrage = function () {
         return obj;
     };
     //配置弹幕对象的属性
+    //长和宽应该取自适应浏览器的长和宽而不是固定的长和宽
     addStyle = (obj, config) => {
-        obj.style.color = config.color || "black";
+        obj.style.color = config.color;
         obj.style.font = '';
         obj.innerText = config.text;
-        obj.style.top = Math.random() * screen.availHeight + 'px';
+        obj.style.top = Math.random() * window.innerHeight + 'px';
     };
     //添加动画规则
     addAnimationRule = () => {
         let style = document.createElement('style');
         document.head.appendChild(style);
         style.type = 'text/css';
-        let start = screen.availWidth;  //初始位置[屏幕的宽度]
-        let end = 0;    //结束位置
+        let start = config.start;  //初始位置[屏幕的宽度]
+        let end = config.end;    //结束位置
         let from = `from { -webkit-transform: translateX(${start}px); }`;
         let to = `to {  -webkit-transform: translateX(${end}px); }`;
         style.sheet.insertRule(`@-webkit-keyframes animation { ${from} ${to} }`, 0);
     };
     //监听 css 动画结束,并且在结束的时候将弹幕移除
     animationend = (event) => {
-        document.getElementById("danmu").removeChild(event.target);
+        // document.getElementById("danmu").removeChild(event.target);
     };
     //获取画布的大小
     getCanvas = () => document.getElementById("canvas").getBoundingClientRect();
     //获取弹幕
     this.getDanMu = () => {
         flag = setInterval(() => {
-            let temp = offlineDanmu.pop();
+            temp = offlineDanmu.pop();
             if (temp !== undefined) {
                 core(temp)
             }
--- app/main.js
+++ app/main.js
@@ -4,7 +4,18 @@ const barrage = require('./barrage');
 //配置信息
 import './main.css';
 //加载弹幕
-new barrage().getDanMu()
+let config = {
+    'text': '',        //文字
+    'color': 'green',  //颜色
+    'img': '',         //图片
+    'pic':'http://127.0.0.1:8888/test/timg.jpeg',
+    'start': window.innerWidth,       //起始位置
+    'end': 0          //结束位置
+
+};
+let barrageObj = new barrage(config);
+barrageObj.getDanMu();
+
 
 
 
